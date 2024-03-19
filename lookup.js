b8473c843d8f63b4fc1ba2eed39de907cc42edb2
HCCDanny <117913283+HCCDanny@users.noreply.github.com>
2024-03-19 16:01:59 +0000

    Create lookup.js

---
+++ lookup.js
@@ -0,0 +1,31 @@
+function createDataRetriever() {
+  return {
+      isLoading: false,
+      isData: false,
+      isError: false,
+      missedStreets: [],
+      getData() {
+          this.isLoading = true;
+          fetch('https://raw.githubusercontent.com/hull-city-council/bin-collection-missed-streets/main/missed-streets.json')
+              .then((response) => {
+                    if (response.ok) {
+                     return response.json();
+                    }
+              })
+              .then((data) => {
+                    if (data[0].events < 1) {
+                      this.isLoading = false;
+                    }
+                  this.missedStreets = data[0].streets;
+                  this.isData = true;
+                  this.isLoading = false;
+              })
+              .catch((error) => {
+                this.isError = true;
+                this.isLoading = false;
+                this.isData = true;
+                console.log(error);
+              });
+      }
+  }
+}
